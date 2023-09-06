776fbdd03c8a568d61285d86f0f1a5df86299155
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-09-06 22:22:21 +0530

    updateed the file

--- Code/streamlit_v2.py
+++ Code/streamlit_v2.py
@@ -284,10 +284,11 @@ with tab3:
         main()
 with tab4:
     import streamlit as st
+    import chromadb
     from langchain.llms import OpenAI
     from langchain.text_splitter import CharacterTextSplitter
     from langchain.embeddings import OpenAIEmbeddings
-    from langchain.vectorstores import Pinecone
+    from langchain.vectorstores import Chroma
     from langchain.chains import RetrievalQA
 
     def generate_response(uploaded_file, openai_api_key, query_text):
